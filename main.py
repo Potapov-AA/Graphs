ab8069f0d537cd1cad40421e5788cd403c0cb66c
JESTER <jestterr@mail.ru>
2022-12-19 16:32:21 +0300

    Вывод уже нового дизайна

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
+import sys
 from app import App
-
-
+from PyQt5 import QtWidgets
+        
+    
 def main():
-    app = App()
-    app.mainloop()
-
-
+    app = QtWidgets.QApplication(sys.argv)
+    window = App()
+    window.show()
+    app.exec_()
+    
 if __name__ == "__main__":
     main()
